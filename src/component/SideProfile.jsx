d5937375d443daad5639cb2ce33e247d472fc8c2
osj1405 <osj1405@naver.com>
2025-05-30 10:17:23 +0900

    update side profile

--- src/component/SideProfile.jsx
+++ src/component/SideProfile.jsx
@@ -9,6 +9,7 @@ export default function SideProfile({
     let navigate = useNavigate();
     const params = useParams(); 
     const [id, setId] = useState("pumupcld");
+    const [nickname, setNickName] = useState("수진");
     const [profileImage, setProfileImage] = useState(myImage);
 
     const handleProfileImage = (src) => {
@@ -21,7 +22,13 @@ export default function SideProfile({
                 <div className={styles.profileImageContainer}>
                     <img src={profileImage} alt="profile" className={styles.profileImage}/>
                 </div>                        
-                <p className={styles.id} onClick={()=>{navigate(`/mypage/:${params.id}`)}}>{id}</p>
+                <p 
+                    className={styles.id} 
+                    onClick={()=>{navigate(`/mypage/:${params.id}`)}}
+                    >{id}</p>
+                <p 
+                    className={styles.nickname}
+                    onClick={() => navigate(`/mypage/:${params.id}`)}>{nickname}</p>
                 <div className={styles.tagContainer}>
                     <p className={styles.tag}>#lovely</p>
                     <p className={styles.tag}>#Hip</p>
