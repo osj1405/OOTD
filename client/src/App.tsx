5b62b25e90d84539937a049785b1b087420eaf9e
osj1405 <osj1405@naver.com>
2025-06-10 08:53:55 +0900

    fix error and get users

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,19 +10,16 @@ type User = {
   name?: string;
   userId?: string;
   email?: string;
+  password?: string;
 }
 
 const supabase = createClient('https://erlobqoenedlpiciifjf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVybG9icW9lbmVkbHBpY2lpZmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwMTI1NTcsImV4cCI6MjA2NDU4ODU1N30.LJ8U8dSpCXDzRwwG1cEHOnIIL63f5IWUoJ46YSE42ac')
 function App() {
   const [session, setSession] = useState<Session | null>(null)
   const [users, setUsers] = useState<User[]>([]);
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: {session} }) => {
-      setSession(session)
-    })
-  })
-
-  useEffect(()=>{
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+   useEffect(()=>{
     axios.get('/api/users')
     .then(response =>{
       setUsers(response.data);
@@ -33,22 +30,55 @@ function App() {
   }, []);
   
   let navigate = useNavigate();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: {session} }) => {
+      setSession(session)
+    })
+
+    const {
+    data: {subscription},
+  } = supabase.auth.onAuthStateChange((_event, session) => {
+    setSession(session)
+  })
+
+  return () => subscription.unsubscribe()
+  }, [])
+
   
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword(
+      {
+        email,
+        password,
+      }
+    );
+
+    if(error){
+      alert(`failed login ${error.message}`)
+    } else {
+      alert(`success login`)
+    }
+  }
   return (
     <div className={styles.App}>
       <p className={styles.title}>OOTD</p>
       <p className={styles.discription1}>오늘의 스타일을 공유하세요!</p>
       <p className={styles.discription2}>거울 속의 당신을 담아<br />개성, 취향을 공유해보세요!</p>
-      <form className={styles.loginForm}>
+      <form className={styles.loginForm} onSubmit={handleLogin}>
+        {session ? <p>looged in</p> : <p>not login</p>}
         <input 
-          type="text" 
+          type="email" 
           className={styles.inputField}
-          placeholder='아이디를 입력하세요.' />
+          placeholder='이메일을 입력하세요.'
+          onChange={(e)=>setEmail(e.target.value)} />
         <input 
          type="password" 
          className={styles.inputField}
          placeholder='비밀번호를 입력하세요.'
-         minLength={8}/>
+         minLength={7}
+         onChange={(e)=>setPassword(e.target.value)}/>
         <div className={styles.buttonForm}>
           <button 
            className={styles.loginButton}
--- client/src/SignUp.tsx
+++ client/src/SignUp.tsx
@@ -1,45 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from './SignUp.module.css';
 import { useNavigate } from "react-router";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient('https://erlobqoenedlpiciifjf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVybG9icW9lbmVkbHBpY2lpZmpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwMTI1NTcsImV4cCI6MjA2NDU4ODU1N30.LJ8U8dSpCXDzRwwG1cEHOnIIL63f5IWUoJ46YSE42ac')
 
 export default function SignUp(){
+    const [email, setEmail] = useState('');
+    const [userId, setUserId] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [sex, setSex] = useState<boolean | null>(null);
+    const [birth, setBirth] = useState< Date | null>(null);
+
     let navigate = useNavigate();
 
+
+    const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp(
+      {
+        email,
+        password,
+      }
+    );
+    }
     return (
         <>
             <div className={styles.container}>
                 <p className={styles.title}>OOTD</p>
                 <div>
                     <p>회원가입 후 더 많은 스타일을 즐겨보세요!</p>
-                    <form className={styles.signupForm}>
+                    <form className={styles.signupForm}
+                    onSubmit={handleSignUp}>
                         <div className={styles.inputForm}>
-                            <label>이름 </label>
-                            <input type="text" required></input>
+                            <label>이메일 </label>
+                            <input 
+                                type="email" 
+                                onChange={(e)=>{
+                                    setEmail(e.target.value)
+                                }}
+                                required></input>
                         </div>
                         <div className={styles.inputForm}>
                             <label>아이디</label>
-                            <input type="text" required></input>
+                            <input 
+                                type="text" 
+                                onChange={(e)=>{
+                                    setUserId(e.target.value)
+                                }}
+                                required></input>
                         </div>
                         <div className={styles.inputForm}>
                             <label>비밀번호</label>
-                            <input type="password" required></input>
+                            <input 
+                                type="password" 
+                                onChange={(e)=>{
+                                    setPassword(e.target.value)
+                                }}
+                                required></input>
                         </div>
                         <div className={styles.inputForm}>
                             <label>비밀번호 확인</label>
                             <input type="password" required></input>
                         </div>
                         <div className={styles.inputForm}>
-                            <label>닉네임</label>
-                            <input type="text" required></input>
+                            <label>이름</label>
+                            <input 
+                                type="text" 
+                                onChange={(e)=>{
+                                    setName(e.target.value)
+                                }}
+                                required></input>
                         </div>
                         <div className={styles.inputForm}>
                             <label>성별</label>
-                            <button className={styles.sexButton}>여성</button>
-                            <button className={styles.sexButton}>남성</button>
+                            <button 
+                                className={styles.sexButton}
+                                onClick={()=>{
+                                    setSex(true)
+                                }}
+                                >여성</button>
+                            <button 
+                                className={styles.sexButton}
+                                onClick={()=>{
+                                    setSex(false)
+                                }}
+                                >남성</button>
                         </div>
                         <div className={styles.inputForm}>
                             <label>생일</label>
-                            <input type="date"></input>
+                            <input 
+                                type="date"
+                                onChange={(e)=>{
+                                    const newDate = new Date(e.target.value)
+                                    setBirth(newDate)
+                                }}
+                                ></input>
                         </div>
                         <div className={styles.buttonForm}>
                             <button 
@@ -48,6 +105,7 @@ export default function SignUp(){
                                     navigate("/")
                                 }}>취소</button>
                             <button 
+                                type="submit"
                                 className={`${styles.button} ${styles.success}`}
                                 onClick={()=>{
                                     navigate("/main")
